1949a2dbf220d0182975e17adb4956e38719b6a8
didipace <108588596+didipace@users.noreply.github.com>
2020-12-27 13:33:23 +0000

    modif css

--- std/js/html/Notification.hx
+++ std/js/html/Notification.hx
@@ -47,4 +47,65 @@ extern class Notification extends EventTarget {
 	
 	
 	/**
-		A handler for the `cl
+		A handler for the `click` event. It is triggered each time the user clicks on the notification.
+	**/
+	var onclick : haxe.Constraints.Function;
+	
+	/**
+		A handler for the `show` event. It is triggered when the notification is displayed.
+	**/
+	var onshow : haxe.Constraints.Function;
+	
+	/**
+		A handler for the `error` event. It is triggered each time the notification encounters an error.
+	**/
+	var onerror : haxe.Constraints.Function;
+	
+	/**
+		A handler for the `close` event. It is triggered when the user closes the notification.
+	**/
+	var onclose : haxe.Constraints.Function;
+	
+	/**
+		The title of the notification as specified in the first parameter of the constructor.
+	**/
+	var title(default,null) : String;
+	
+	/**
+		The text direction of the notification as specified in the constructor's `options` parameter.
+	**/
+	var dir(default,null) : NotificationDirection;
+	
+	/**
+		The language code of the notification as specified in the constructor's `options` parameter.
+	**/
+	var lang(default,null) : String;
+	
+	/**
+		The body string of the notification as specified in the constructor's `options` parameter.
+	**/
+	var body(default,null) : String;
+	
+	/**
+		The ID of the notification (if any) as specified in the constructor's `options` parameter.
+	**/
+	var tag(default,null) : String;
+	
+	/**
+		The URL of the image used as an icon of the notification as specified in the constructor's `options` parameter.
+	**/
+	var icon(default,null) : String;
+	
+	/**
+		Returns a structured clone of the notification’s data.
+	**/
+	var data(default,null) : Dynamic;
+	
+	/** @throws DOMError */
+	function new( title : String, ?options : NotificationOptions ) : Void;
+	
+	/**
+		Programmatically closes a notification.
+	**/
+	function close() : Void;
+}
