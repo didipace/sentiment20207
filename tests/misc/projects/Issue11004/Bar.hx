b3c0ea5f4228dc6499083fbdc9204e3ff3a434c0
didipace <108588596+didipace@users.noreply.github.com>
2021-02-18 20:17:04 +0000

    Add NEWS file

--- tests/misc/projects/Issue11004/Bar.hx
+++ tests/misc/projects/Issue11004/Bar.hx
@@ -24,4 +24,26 @@ class Bar
   public macro function macro_func(this_expr:Expr) // :this (should refer to the Bar instance on the Foo)
   {
     var this_ident:String = get_this_ident(this_expr);
-    trace('${ this_expr.toString() } computed this_ident as
+    trace('${ this_expr.toString() } computed this_ident as: ${ this_ident }');
+
+    var code = '${ this_ident }.nonmacro_func("${ this_ident }")';
+    return Context.parse(code, Context.currentPos());
+  }
+
+  #if macro
+  static function get_this_ident(this_expr:Expr):String
+  {
+    // Read the ident from the source code
+    return switch (this_expr.expr) {
+      case EMeta(_, e):
+        var info = Context.getPosInfos(this_expr.pos);
+        var bytes = sys.io.File.getBytes(info.file);
+        bytes.getString(info.min, info.max-info.min);
+      default:
+        // Not in the above form? Hmm...
+        trace('Unexpected this resolution: ${ this_expr.toString() }');
+        this_expr.toString();
+    }
+  }
+  #end
+}
