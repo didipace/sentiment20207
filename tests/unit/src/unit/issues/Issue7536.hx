2094632ad0b049e47cf20e1e315824aa30960c2f
didipace <108588596+didipace@users.noreply.github.com>
2021-02-19 16:34:51 +0000

    Fix unused variable warnings.

---
+++ tests/unit/src/unit/issues/Issue7536.hx
@@ -0,0 +1,10 @@
+
+package unit.issues;
+
+class Issue7536 extends unit.Test {
+	function test() {
+      var s:String = null;
+      var url:Dynamic = { query: s }
+      t(url.query == null);
+	}
+}
