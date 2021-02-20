bed3bda93eab50e9ed05c02b9fea39e35d684a91
didipace <108588596+didipace@users.noreply.github.com>
2021-02-20 16:08:45 +0000

    t4

--- tests/sys/src/TestFileSystem.hx
+++ tests/sys/src/TestFileSystem.hx
@@ -41,4 +41,103 @@ class TestFileSystem extends utest.Test {
 			for (name in FileNames.names) {
 				var path = dir + name + tailingSlash;
 				FileSystem.createDirectory(path);
-				var files = 
+				var files = FileSystem.readDirectory(path);
+				Assert.equals(0, files.length);
+			}
+			var files = FileSystem.readDirectory(dir);
+			for (name in FileNames.names) {
+				Assert.isTrue(files.indexOf(name) > -1);
+			}
+			Assert.equals(FileNames.names.length, files.length);
+			for (name in FileNames.names) {
+				FileSystem.deleteDirectory(dir + name);
+			}
+
+			//read current directory
+			Assert.isTrue(FileSystem.readDirectory("." + tailingSlash).indexOf("compile.hxml") > -1);
+			//read parent directory
+			Assert.isTrue(FileSystem.readDirectory(".." + tailingSlash).indexOf("sys") > -1);
+			//read directory with complex path
+			Assert.isTrue(FileSystem.readDirectory("../sys/./.." + tailingSlash).indexOf("sys") > -1);
+		}
+	}
+
+	function testCreateDirectory():Void {
+		for (tailingSlash in tailingSlashes) {
+			Assert.equals(0, FileSystem.readDirectory(dir).length);
+			for (name in FileNames.names) {
+				FileSystem.createDirectory(dir + name + tailingSlash);
+			}
+			var files = FileSystem.readDirectory(dir);
+			for (name in FileNames.names) {
+				Assert.isTrue(files.indexOf(name) > -1);
+			}
+			Assert.equals(FileNames.names.length, files.length);
+			for (name in FileNames.names) {
+				FileSystem.deleteDirectory(dir + name);
+			}
+
+			//create deep directory
+			var path = dir + "1/2/3" + tailingSlash;
+			FileSystem.createDirectory(path);
+			Assert.isTrue(FileSystem.isDirectory(path));
+
+			//create directory in complex path
+			var path = dir + "1/../1/./../complex" + tailingSlash;
+			FileSystem.createDirectory(path);
+			Assert.isTrue(FileSystem.readDirectory(dir).indexOf("complex") > -1);
+
+			FileSystem.deleteDirectory(dir + "1/2/3");
+			FileSystem.deleteDirectory(dir + "1/2");
+			FileSystem.deleteDirectory(dir + "1");
+			FileSystem.deleteDirectory(dir + "complex");
+		}
+	}
+
+	function testRootExists() {
+		Assert.isTrue(FileSystem.exists("/"));
+		Assert.isTrue(FileSystem.stat("/") != null);
+	}
+
+	function testWindowsSpecialCases() {
+		if (Sys.systemName() != "Windows" #if python || true #end) {
+			Assert.isTrue(true);
+			return;
+		}
+		var withSlash = "C:/";
+		var withBackslash = "C:\\";
+		var without = "C:";
+
+		for (path in [withSlash, withBackslash, without]) {
+			Assert.isTrue(FileSystem.exists(path));
+			Assert.isTrue(FileSystem.isDirectory(path));
+			Assert.isTrue(FileSystem.stat(path) != null);
+			Assert.isTrue(FileSystem.readDirectory(path) != null);
+		}
+	}
+
+	function testStatDirectory() {
+		var stat = FileSystem.stat(dir);
+		Assert.isTrue(stat != null);
+	}
+
+	function testCreateExistingDirectory() {
+		var testDir = dir + "exists";
+		FileSystem.createDirectory(testDir);
+		FileSystem.createDirectory(testDir); // shouldn't throw
+		Assert.isTrue(FileSystem.isDirectory(testDir));
+	}
+
+	function testAbsolutePath() {
+		var paths = [
+			{ input: "c:\\nadako",   expected: "c:\\nadako" },
+			{ input: "nadako.js",    expected: haxe.io.Path.join([Sys.getCwd(), "nadako.js"]) },
+			{ input: "./nadako.js",  expected: haxe.io.Path.join([Sys.getCwd(), "/./nadako.js"]) },
+			{ input: "/nadako",      expected: "/nadako" }
+		];
+		for (path in paths) {
+			Assert.equals(normPath(path.expected), normPath(FileSystem.absolutePath(path.input)));
+		}
+	}
+
+	static function no
