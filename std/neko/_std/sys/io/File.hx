503d0994997464ef14cc9c8bf0ae4a05267e041a
didipace <108588596+didipace@users.noreply.github.com>
2021-02-24 07:33:09 +0000

    Update BUILDING.md

--- std/neko/_std/sys/io/File.hx
+++ std/neko/_std/sys/io/File.hx
@@ -16,4 +16,62 @@
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+package sys.io;
+
+enum FileHandle {}
+
+@:coreApi class File {
+	public static function getContent(path:String):String {
+		return new String(file_contents(untyped path.__s));
+	}
+
+	public static function getBytes(path:String):haxe.io.Bytes {
+		return neko.Lib.bytesReference(getContent(path));
+	}
+
+	public static function saveContent(path:String, content:String):Void {
+		var f = write(path);
+		f.writeString(content);
+		f.close();
+	}
+
+	public static function saveBytes(path:String, bytes:haxe.io.Bytes):Void {
+		var f = write(path);
+		f.write(bytes);
+		f.close();
+	}
+
+	public static function read(path:String, binary:Bool = true):FileInput {
+		return untyped new FileInput(file_open(path.__s, (if (binary) "rb" else "r").__s));
+	}
+
+	public static function write(path:String, binary:Bool = true):FileOutput {
+		return untyped new FileOutput(file_open(path.__s, (if (binary) "wb" else "w").__s));
+	}
+
+	public static function append(path:String, binary:Bool = true):FileOutput {
+		return untyped new FileOutput(file_open(path.__s, (if (binary) "ab" else "a").__s));
+	}
+
+	public static function update(path:String, binary:Bool = true):FileOutput {
+		if (!FileSystem.exists(path)) {
+			write(path).close();
+		}
+		return untyped new FileOutput(file_open(path.__s, (if (binary) "rb+" else "r+").__s));
+	}
+
+	public static function copy(srcPath:String, dstPath:String):Void {
+		var s = read(srcPath, true);
+		var d = write(dstPath, true);
+		d.writeInput(s);
+		s.close();
+		d.close();
+	}
+
+	private static var file_contents = neko.Lib.load("std", "file_contents", 1);
+	private static var file_open = neko.Lib.load("std", "file_open", 2);
+}
