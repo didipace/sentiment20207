bca29dcfd418421bcb8fd97e0b8a728f373540b5
didipace <108588596+didipace@users.noreply.github.com>
2021-03-24 15:38:03 +0000

    USB: serial: cp210x: add IDs for CEL MeshConnect USB Stick

--- libs/pcre/pcre.ml
+++ libs/pcre/pcre.ml
@@ -90,4 +90,121 @@ let coll_icflag icflag flag = int_of_cflag flag lor icflag
 let cflags flags = List.fold_left coll_icflag 0 flags
 
 let cflag_of_int = function
-  | 0
+  | 0x0001 -> `CASELESS
+  | 0x0002 -> `MULTILINE
+  | 0x0004 -> `DOTALL
+  | 0x0008 -> `EXTENDED
+  | 0x0010 -> `ANCHORED
+  | 0x0020 -> `DOLLAR_ENDONLY
+  | 0x0040 -> `EXTRA
+  | 0x0200 -> `UNGREEDY
+  | 0x0800 -> `UTF8
+  | 0x1000 -> `NO_AUTO_CAPTURE
+  | 0x2000 -> `NO_UTF8_CHECK
+  | 0x4000 -> `AUTO_CALLOUT
+  | 0x40000 -> `FIRSTLINE
+  | 0x20000000 -> `UCP
+  | _ -> failwith "Pcre.cflag_list: unknown compilation flag"
+
+let all_cflags =
+  [
+    0x0001; 0x0002; 0x0004; 0x0008; 0x0010; 0x0020;
+    0x0040; 0x0200; 0x0800; 0x1000; 0x2000; 0x4000; 0x40000;
+	0x20000000
+  ]
+
+let cflag_list icflags =
+  let coll flag_list flag =
+    if icflags land flag <> 0 then cflag_of_int flag :: flag_list
+    else flag_list in
+  List.fold_left coll [] all_cflags
+
+
+(* Runtime flags *)
+
+type rflag =
+  [
+  | `ANCHORED
+  | `NOTBOL
+  | `NOTEOL
+  | `NOTEMPTY
+  | `PARTIAL
+  ]
+
+let int_of_rflag = function
+  | `ANCHORED -> 0x0010
+  | `NOTBOL -> 0x0080
+  | `NOTEOL -> 0x0100
+  | `NOTEMPTY -> 0x0400
+  | `PARTIAL -> 0x8000
+
+let coll_irflag irflag flag = int_of_rflag flag lor irflag
+let rflags flags = List.fold_left coll_irflag 0 flags
+
+let rflag_of_int = function
+  | 0x0010 -> `ANCHORED
+  | 0x0080 -> `NOTBOL
+  | 0x0100 -> `NOTEOL
+  | 0x0400 -> `NOTEMPTY
+  | 0x8000 -> `PARTIAL
+  | _ -> failwith "Pcre.rflag_list: unknown runtime flag"
+
+let all_rflags = [0x0010; 0x0080; 0x0100; 0x0400; 0x8000]
+
+let rflag_list irflags =
+  let coll flag_list flag =
+    if irflags land flag <> 0 then rflag_of_int flag :: flag_list
+    else flag_list in
+  List.fold_left coll [] all_rflags
+
+
+(* Information on the PCRE-configuration (build-time options) *)
+
+external pcre_version : unit -> string = "pcre_version_stub"
+
+external pcre_config_utf8 : unit -> bool = "pcre_config_utf8_stub" [@@noalloc]
+
+external pcre_config_newline :
+  unit -> char = "pcre_config_newline_stub" [@@noalloc]
+
+external pcre_config_link_size :
+  unit -> int = "pcre_config_link_size_stub" [@@noalloc]
+
+external pcre_config_match_limit :
+  unit -> int = "pcre_config_match_limit_stub" [@@noalloc]
+
+external pcre_config_match_limit_recursion :
+  unit -> int = "pcre_config_match_limit_recursion_stub" [@@noalloc]
+
+external pcre_config_stackrecurse :
+  unit -> bool = "pcre_config_stackrecurse_stub" [@@noalloc]
+
+let version = pcre_version ()
+let config_utf8 = pcre_config_utf8 ()
+let config_newline = pcre_config_newline ()
+let config_link_size = pcre_config_link_size ()
+let config_match_limit = pcre_config_match_limit ()
+let config_match_limit_recursion = pcre_config_match_limit_recursion ()
+let config_stackrecurse = pcre_config_stackrecurse ()
+
+
+(* Information on patterns *)
+
+type firstbyte_info =
+  [ `Char of char
+  | `Start_only
+  | `ANCHORED ]
+
+type study_stat =
+  [ `Not_studied
+  | `Studied
+  | `Optimal ]
+
+type regexp
+
+external options : regexp -> icflag = "pcre_options_stub"
+external size : regexp -> int = "pcre_size_stub"
+external studysize : regexp -> int = "pcre_studysize_stub"
+external capturecount : regexp -> int = "pcre_capturecount_stub"
+external backrefmax : regexp -> int = "pcre_backrefmax_stub"
+ext
