ffc2c282a9dcb1eda315f63783ea0b721f42fcdf
didipace <108588596+didipace@users.noreply.github.com>
2021-04-12 14:10:12 +0000

    Update config.yaml

--- src/context/display/documentSymbols.ml
+++ src/context/display/documentSymbols.ml
@@ -49,4 +49,101 @@ let collect_module_symbols mname with_locals (pack,decls) =
 		| FVar(_,eo) ->
 			add_field (
 				if parent_kind = EnumAbstract && not (List.mem_assoc AStatic cff_access) then EnumMember
-				else if (List.mem_assoc AInline cff_access) 
+				else if (List.mem_assoc AInline cff_access) then Constant
+				else Field
+			);
+			if with_locals then expr_opt field_parent eo
+		| FFun f ->
+			add_field (
+				if fst cff_name = "new" then Constructor
+				else if ((parent_kind = EnumAbstract or parent_kind = Abstract) && Meta.has_one_of [Meta.Op; Meta.ArrayAccess; Meta.Resolve] cff_meta) then Operator
+				else Method
+			);
+			if with_locals then func field_parent f
+		| FProp(_,_,_,eo) ->
+			add_field Property;
+			if with_locals then expr_opt field_parent eo
+	in
+	let field parent parent_kind cff =
+		field' parent parent_kind cff.cff_name cff.cff_kind cff.cff_access cff.cff_pos cff.cff_meta
+	in
+	let type_decls = Hashtbl.create 0 in
+	List.iter (fun (td,p) ->
+		let get_decl_path d =
+			let module_name = Path.module_name_of_file p.pfile in
+			let type_name = fst d.d_name in
+			let is_primary_type = type_name = module_name in
+			let type_path = if is_primary_type then pack else pack @ [module_name] in
+			type_path, type_name
+		in
+		let string_of_path l = String.concat "." l in
+		let add_type d kind =
+			let type_path, type_name = get_decl_path d in
+			Hashtbl.add type_decls type_name ();
+			add type_name kind p (string_of_path type_path) (is_deprecated d.d_meta);
+			string_of_path (type_path @ [type_name])
+		in
+		match td with
+		| EImport _ | EUsing _ ->
+			()
+		| EClass d ->
+			let kind = if List.mem HInterface d.d_flags then Interface else Class in
+			let parent = add_type d kind in
+			List.iter (field parent kind) d.d_data
+		| EEnum d ->
+			let parent = add_type d Enum in
+			List.iter (fun ef ->
+				add (fst ef.ec_name) EnumMember ef.ec_pos parent (is_deprecated ef.ec_meta)
+			) d.d_data
+		| ETypedef d ->
+			(match d.d_data with
+			| CTAnonymous fields,_ ->
+				let parent = add_type d Struct in
+				List.iter (field parent Struct) fields
+			| _ ->
+				ignore(add_type d TypeAlias)
+			)
+		| EAbstract d ->
+			let kind = if List.mem AbEnum d.d_flags then EnumAbstract else Abstract in
+			let parent = add_type d kind in
+			List.iter (field parent kind) d.d_data
+		| EStatic d ->
+			let path, name = get_decl_path d in
+			let dotpath = string_of_path path in
+			field' dotpath Class d.d_name d.d_data d.d_flags p d.d_meta
+	) decls;
+	begin match mname with
+	| Some(file,mname) when not (Hashtbl.mem type_decls mname) ->
+		add mname Module {pfile = file; pmin = 0; pmax = 0} (String.concat "." pack) false
+	| _ ->
+		()
+	end;
+	l
+
+module Printer = struct
+	open Json
+	open DisplayTypes.SymbolKind
+	open DisplayTypes.SymbolInformation
+
+	let print_module_symbols com symbols filter =
+		let regex = Option.map Str.regexp_case_fold filter in
+		let reported = Hashtbl.create 0 in
+		let add si =
+			if Hashtbl.mem reported si.pos then false
+			else begin
+				let b = match regex with
+					| None -> true
+					| Some regex -> (try ignore(Str.search_forward regex si.name 0); true with Not_found -> false)
+				in
+				Hashtbl.replace reported si.pos true;
+				b
+			end
+		in
+		let ja = List.fold_left (fun acc (file,l) ->
+			let jl = ExtList.List.filter_map (fun si ->
+				if not (add si) then
+					None
+				else begin
+					let l =
+						("name",JString si.name) ::
+						("kind",JInt (t
