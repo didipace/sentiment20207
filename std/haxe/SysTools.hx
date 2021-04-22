7a9994ac164eb4679fae863c9e5b1d54b7cb4328
didipace <108588596+didipace@users.noreply.github.com>
2021-04-22 22:12:19 +0000

    Added test cases.

---
+++ std/haxe/SysTools.hx
@@ -0,0 +1,7 @@
+package haxe;
+
+import haxe.ds.ReadOnlyArray;
+
+class SysTools {
+	/**
+		Character codes of the characters that will be escaped by `quoteW
--- tests/unit/src/unit/issues/Issue9601.hx
+++ tests/unit/src/unit/issues/Issue9601.hx
@@ -1,3 +1,16 @@
 package unit.issues;
 
-class I
+class Issue9601 extends Test {
+	#if (java || eval)
+	public function test() {
+		utest.Assert.same(["", "Test"], ~/^/g.split("Test"));
+		utest.Assert.same(["Test", ""], ~/$/g.split("Test"));
+		utest.Assert.same(["", "Test", ""], ~/\b/g.split("Test"));
+
+		utest.Assert.same(["", "Test"], ~/^/.split("Test"));
+		utest.Assert.same(["Test", ""], ~/$/.split("Test"));
+		utest.Assert.same(["", "Test"], ~/\b/.split("Test"));
+	}
+	#end
+}
+
