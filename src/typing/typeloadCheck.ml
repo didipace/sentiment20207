965a90384d5b95d69bc3017ca4b9a2110b0bcc6f
didipace <108588596+didipace@users.noreply.github.com>
2021-04-24 12:24:45 +0000

    added missing includes

--- src/typing/typeloadCheck.ml
+++ src/typing/typeloadCheck.ml
@@ -5,4 +5,88 @@
 	This program is free software; you can redistribute it and/or
 	modify it under the terms of the GNU General Public License
 	as published by the Free Software Foundation; either version 2
-	of the License, or (at your option) any lat
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *)
+
+(* Various checks performed while loading types. *)
+
+open Globals
+open Ast
+open Type
+open Typecore
+open DisplayException
+open DisplayTypes
+open DisplayMode
+open CompletionItem
+open CompletionModuleKind
+open CompletionModuleType
+open CompletionResultKind
+open Common
+open Error
+
+exception Build_canceled of build_state
+
+let is_generic_parameter ctx c =
+	(* first check field parameters, then class parameters *)
+	let name = snd c.cl_path in
+	try
+		ignore(lookup_param name ctx.curfield.cf_params);
+		has_class_field_flag ctx.curfield CfGeneric
+	with Not_found -> try
+		ignore(lookup_param name ctx.type_params);
+		(match ctx.curclass.cl_kind with | KGeneric -> true | _ -> false);
+	with Not_found ->
+		false
+
+let valid_redefinition ctx map1 map2 f1 t1 f2 t2 = (* child, parent *)
+	let valid t1 t2 =
+		Type.unify t1 t2;
+		if is_null t1 <> is_null t2 || ((follow t1) == t_dynamic && (follow t2) != t_dynamic) then raise (Unify_error [Cannot_unify (t1,t2)]);
+	in
+	let open OptimizerTexpr in
+	begin match PurityState.get_purity_from_meta f2.cf_meta,PurityState.get_purity_from_meta f1.cf_meta with
+		| PurityState.Pure,PurityState.MaybePure -> f1.cf_meta <- (Meta.Pure,[EConst(Ident "expect"),f2.cf_pos],null_pos) :: f1.cf_meta
+		| PurityState.ExpectPure p,PurityState.MaybePure -> f1.cf_meta <- (Meta.Pure,[EConst(Ident "expect"),p],null_pos) :: f1.cf_meta
+		| _ -> ()
+	end;
+	let t1, t2 = (match f1.cf_params, f2.cf_params with
+		| [], [] -> t1, t2
+		| l1, l2 when List.length l1 = List.length l2 ->
+			let to_check = ref [] in
+			(* TPTODO: defaults *)
+			let monos = List.map2 (fun tp1 tp2 ->
+				(match follow tp1.ttp_type, follow tp2.ttp_type with
+				| TInst ({ cl_kind = KTypeParameter ct1 } as c1,pl1), TInst ({ cl_kind = KTypeParameter ct2 } as c2,pl2) ->
+					(match ct1, ct2 with
+					| [], [] -> ()
+					| _, _ when List.length ct1 = List.length ct2 ->
+						(* if same constraints, they are the same type *)
+						let check monos =
+							List.iter2 (fun t1 t2  ->
+								try
+									let t1 = apply_params l1 monos (apply_params c1.cl_params pl1 (map2 t1)) in
+									let t2 = apply_params l2 monos (apply_params c2.cl_params pl2 (map1 t2)) in
+									type_eq EqStrict t1 t2
+								with Unify_error l ->
+									raise (Unify_error (Unify_custom "Constraints differ" :: l))
+							) ct1 ct2
+						in
+						to_check := check :: !to_check;
+					| _ ->
+						raise (Unify_error [Unify_custom "Different number of constraints"]))
+				| _ -> ());
+				TInst (mk_class null_module ([],tp1.ttp_name) null_pos null_pos,[])
+			) l1 l2 in
+			List.iter (fun f -> f monos) !to_check;
+			apply_params l1 monos t1, apply_params l2 monos t2
+		| _  ->
+			(* ignore type params, will
