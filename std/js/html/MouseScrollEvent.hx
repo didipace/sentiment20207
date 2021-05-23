53122400a5770c7846d38250374075ab69346e50
didipace <108588596+didipace@users.noreply.github.com>
2021-05-23 07:53:35 +0000

    first git

--- std/js/html/MouseScrollEvent.hx
+++ std/js/html/MouseScrollEvent.hx
@@ -11,4 +11,32 @@
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+// This file is generated from mozilla\MouseScrollEvent.webidl. Do not edit!
+
+package js.html;
+
+/**
+	The `MouseScrollEvent` interface represents events that occur due to the user moving a mouse wheel or similar input device.
+
+	Documentation [MouseScrollEvent](https://developer.mozilla.org/en-US/docs/Web/API/MouseScrollEvent) by [Mozilla Contributors](https://developer.mozilla.org/en-US/docs/Web/API/MouseScrollEvent$history), licensed under [CC-BY-SA 2.5](https://creativecommons.org/licenses/by-sa/2.5/).
+
+	@see <https://developer.mozilla.org/en-US/docs/Web/API/MouseScrollEvent>
+**/
+@:native("MouseScrollEvent")
+extern class MouseScrollEvent extends MouseEvent {
+	static inline var HORIZONTAL_AXIS : Int = 1;
+	static inline var VERTICAL_AXIS : Int = 2;
+	
+	var axis(default,null) : Int;
+	
+	function initMouseScrollEvent( type : String, canBubble : Bool = false, cancelable : Bool = false, ?view : Window, detail : Int = 0, screenX : Int = 0, screenY : Int = 0, clientX : Int = 0, clientY : Int = 0, ctrlKey : Bool = false, altKey : Bool = false, shiftKey : Bool = false, metaKey : Bool = false, button : Int = 0, ?relatedTarget : EventTarget, axis : Int = 0 ) : Void;
+}
