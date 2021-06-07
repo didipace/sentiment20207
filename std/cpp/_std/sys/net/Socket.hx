a8d7f7ea19f163e60f16ff374cf3750367e27242
didipace <108588596+didipace@users.noreply.github.com>
2021-06-07 13:40:10 +0000

    Update upgrade.sh

--- std/cpp/_std/sys/net/Socket.hx
+++ std/cpp/_std/sys/net/Socket.hx
@@ -8,4 +8,139 @@
  * and/or sell copies of the Software, and to permit persons to whom the
  * Software is furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be includ
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+package sys.net;
+
+import haxe.io.Error;
+import cpp.NativeSocket;
+import cpp.NativeString;
+import cpp.Pointer;
+
+private class SocketInput extends haxe.io.Input {
+	var __s:Dynamic;
+
+	public function new(s:Dynamic) {
+		__s = s;
+	}
+
+	public override function readByte() {
+		return try {
+			NativeSocket.socket_recv_char(__s);
+		} catch (e:Dynamic) {
+			if (e == "Blocking")
+				throw Blocked;
+			else if (__s == null)
+				throw Custom(e);
+			else
+				throw new haxe.io.Eof();
+		}
+	}
+
+	public override function readBytes(buf:haxe.io.Bytes, pos:Int, len:Int):Int {
+		var r;
+		if (__s == null)
+			throw "Invalid handle";
+		try {
+			r = NativeSocket.socket_recv(__s, buf.getData(), pos, len);
+		} catch (e:Dynamic) {
+			if (e == "Blocking")
+				throw Blocked;
+			else
+				throw Custom(e);
+		}
+		if (r == 0)
+			throw new haxe.io.Eof();
+		return r;
+	}
+
+	public override function close() {
+		super.close();
+		if (__s != null)
+			NativeSocket.socket_close(__s);
+	}
+}
+
+private class SocketOutput extends haxe.io.Output {
+	var __s:Dynamic;
+
+	public function new(s:Dynamic) {
+		__s = s;
+	}
+
+	public override function writeByte(c:Int) {
+		if (__s == null)
+			throw "Invalid handle";
+		try {
+			NativeSocket.socket_send_char(__s, c);
+		} catch (e:Dynamic) {
+			if (e == "Blocking")
+				throw Blocked;
+			else
+				throw Custom(e);
+		}
+	}
+
+	public override function writeBytes(buf:haxe.io.Bytes, pos:Int, len:Int):Int {
+		return try {
+			NativeSocket.socket_send(__s, buf.getData(), pos, len);
+		} catch (e:Dynamic) {
+			if (e == "Blocking")
+				throw Blocked;
+			else if (e == "EOF")
+				throw new haxe.io.Eof();
+			else
+				throw Custom(e);
+		}
+	}
+
+	public override function close() {
+		super.close();
+		if (__s != null)
+			NativeSocket.socket_close(__s);
+	}
+}
+
+@:coreApi
+class Socket {
+	private var __s:Dynamic;
+
+	// We need to keep these values so that we can restore
+	// them if we re-create the socket for ipv6 as in
+	// connect() and bind() below.
+	private var __timeout:Float = 0.0;
+	private var __blocking:Bool = true;
+	private var __fastSend:Bool = false;
+
+	public var input(default, null):haxe.io.Input;
+	public var output(default, null):haxe.io.Output;
+	public var custom:Dynamic;
+
+	public function new():Void {
+		init();
+	}
+
+	private function init():Void {
+		if (__s == null)
+			__s = NativeSocket.socket_new(false);
+		// Restore these values if they changed. This can happen
+		// in connect() and bind() if using an ipv6 address.
+		setTimeout(__timeout);
+		setBlocking(__blocking);
+		setFastSend(__fastSend);
+		input = new SocketInput(__s);
+		output = new SocketOutput(__s);
+	}
+
+	public function close():Void {
+		NativeSocket.socket_close(__
