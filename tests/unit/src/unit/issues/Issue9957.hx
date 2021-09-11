e54972192b3f936b3f0dc4f0e848e1c38b22a201
didipace <108588596+didipace@users.noreply.github.com>
2021-09-11 10:24:27 +0000

    task 4123

--- tests/unit/src/unit/issues/Issue9957.hx
+++ tests/unit/src/unit/issues/Issue9957.hx
@@ -7,4 +7,26 @@ class Issue9957 extends unit.Test {
 
 		try {
 			try stat()
-		
+			catch(_:MyEx) noAssert();
+		} catch(_) {
+			assert("Can't catch exceptions from static closures");
+		}
+
+		try {
+			try inst()
+			catch(_:MyEx) noAssert();
+		} catch(_) {
+			assert("Can't catch exceptions from instance closures");
+		}
+	}
+
+	static function staticExc() {
+		throw new MyEx('');
+	}
+
+	function instanceExc() {
+		throw new MyEx('');
+	}
+}
+
+private class MyEx extends haxe.Exception {}
