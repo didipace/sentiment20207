a12eaed4ed5847e857c22405f02df3e40b9e370e
didipace <108588596+didipace@users.noreply.github.com>
2021-09-30 10:43:10 +0000

    css improvements

--- tests/unit/src/unitstd/haxe/io/Float32Array.unit.hx
+++ tests/unit/src/unitstd/haxe/io/Float32Array.unit.hx
@@ -36,4 +36,43 @@ b2.length == 3;
 
 // check memory sharing
 b2[0] = 0xCC;
-b2[0] == 0
+b2[0] == 0xCC;
+b[1] == 0xCC;
+
+
+// should we allow writing past bounds ?
+try b2[-1] = 0xBB catch( e : Dynamic ) {};
+b[0] == 1;
+
+try b2[3] = 0xBB catch( e : Dynamic ) {};
+b[4] == 5;
+
+
+b.view == b.view; // no alloc
+
+b.view.buffer == b2.view.buffer;
+b.view.byteLength == 20;
+b.view.byteOffset == 0;
+b2.view.byteLength == 12;
+b2.view.byteOffset == 4;
+
+// check sub
+var sub = b.sub(1);
+sub.length == b.length - 1;
+sub[0] == 0xCC;
+sub[0] = 0xDD;
+b[1] == 0xDD;
+
+var sub = b.subarray(2,3);
+sub.length == 1;
+sub[0] == 3;
+sub[0] = 0xEE;
+b[2] == 0xEE;
+
+// from bytes
+var b3 = haxe.io.Float32Array.fromBytes(b.view.buffer, 2*4, 3);
+b3.length == 3;
+for( i in 0...3 )
+	b3[i] == b[i+2];
+b3[0] = b[3] + 1;
+b3[0] == b[2];
