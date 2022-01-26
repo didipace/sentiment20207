bb2a43d27db4bb9053c9d4fdddc3bc73962ac90f
didipace <108588596+didipace@users.noreply.github.com>
2022-01-26 21:09:20 +0000

    version 0.0.12

--- std/cs/_std/sys/thread/Mutex.hx
+++ std/cs/_std/sys/thread/Mutex.hx
@@ -11,4 +11,33 @@
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WAR
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+package sys.thread;
+
+import cs.system.threading.Mutex as NativeMutex;
+
+class Mutex {
+	final native = new NativeMutex();
+
+	public function new():Void {}
+
+	public function acquire():Void {
+		native.WaitOne();
+	}
+
+	public function tryAcquire():Bool {
+		return native.WaitOne(0);
+	}
+
+	public function release():Void {
+		native.ReleaseMutex();
+	}
+}
