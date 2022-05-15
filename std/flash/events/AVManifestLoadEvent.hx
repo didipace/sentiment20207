ea183f464ec8cce2c1b9125b6a32351d045a0a12
didipace <108588596+didipace@users.noreply.github.com>
2022-05-15 18:14:26 +0000

    updated form

--- std/flash/events/AVManifestLoadEvent.hx
+++ std/flash/events/AVManifestLoadEvent.hx
@@ -5,4 +5,10 @@ extern class AVManifestLoadEvent extends Event {
 	@:flash.property var handle(get,never) : Int;
 	@:flash.property var result(get,never) : flash.media.AVResult;
 	@:flash.property var userData(get,never) : Int;
-	function new(?type : String, bubbles : Bool = false, cancelable : Boo
+	function new(?type : String, bubbles : Bool = false, cancelable : Bool = false, inResult : Int = 0, inUserData : Int = 0, inHandle : Int = 0, inDuration : Float = 0) : Void;
+	private function get_duration() : Float;
+	private function get_handle() : Int;
+	private function get_result() : flash.media.AVResult;
+	private function get_userData() : Int;
+	static final AV_MANIFEST_LOAD : String;
+}
