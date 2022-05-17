df43f3aefe99f85936c641a1a002ec43c508692a
didipace <108588596+didipace@users.noreply.github.com>
2022-05-17 22:43:12 +0000

    removed obsolete comments

--- std/flash/_std/Reflect.hx
+++ std/flash/_std/Reflect.hx
@@ -43,4 +43,89 @@
 			if (__in__(getter, o)) {
 				return o[getter]();
 			}
-			r
+			return __in__(field, o) ? o[field] : null;
+		}
+
+	public static function setProperty(o:Dynamic, field:String, value:Dynamic):Void
+		untyped {
+			var setter = 'set_$field';
+			if (__in__(setter, o)) {
+				o[setter](value);
+			} else {
+				o[field] = value;
+			}
+		}
+
+	public inline static function callMethod(o:Dynamic, func:haxe.Constraints.Function, args:Array<Dynamic>):Dynamic
+		untyped {
+			return func.apply(o, args);
+		}
+
+	public static function fields(o:Dynamic):Array<String>
+		untyped {
+			if (o == null)
+				return new Array();
+			var i = 0;
+			var a = [];
+			while (untyped __has_next__(o, i)) {
+				var prop = untyped __forin__(o, i);
+				if (o.hasOwnProperty(prop))
+					a.push(prop);
+			}
+			return a;
+		}
+
+	public static function isFunction(f:Dynamic):Bool
+		untyped {
+			return __typeof__(f) == "function";
+		}
+
+	public static function compare<T>(a:T, b:T):Int {
+		var a:Dynamic = a;
+		var b:Dynamic = b;
+		return (a == b) ? 0 : ((a > b) ? 1 : -1);
+	}
+
+	public static function compareMethods(f1:Dynamic, f2:Dynamic):Bool {
+		return f1 == f2; // VM-level closures
+	}
+
+	public static function isObject(v:Dynamic):Bool
+		untyped {
+			if (v == null)
+				return false;
+			var t = __typeof__(v);
+			if (t == "object") {
+				return !isEnumValue(v);
+			}
+			return (t == "string");
+		}
+
+	public static function isEnumValue(v:Dynamic):Bool {
+		return try v.__enum__ == true catch (e:Dynamic) false;
+	}
+
+	public static function deleteField(o:Dynamic, field:String):Bool
+		untyped {
+			if (o.hasOwnProperty(field) != true)
+				return false;
+			__delete__(o, field);
+			return true;
+		}
+
+	public static function copy<T>(o:Null<T>):Null<T> {
+		if (o == null)
+			return null;
+		var o2:Dynamic = {};
+		for (f in Reflect.fields(o))
+			Reflect.setField(o2, f, Reflect.field(o, f));
+		return o2;
+	}
+
+	@:overload(function(f:Array<Dynamic>->Void):Dynamic {})
+	public static function makeVarArgs(f:Array<Dynamic>->Dynamic):Dynamic {
+		return function(__arguments__) {
+			return f(__arguments__);
+		};
+	}
+}
