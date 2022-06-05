c528cf9ecebbd5b74a912ac76b99f1c77eb9fd91
didipace <108588596+didipace@users.noreply.github.com>
2022-06-05 12:16:19 +0000

    alpha: Add missing RCU idle APIs on idle loop

--- tests/misc/cs/projects/Issue3703/Test.hx
+++ tests/misc/cs/projects/Issue3703/Test.hx
@@ -4,4 +4,12 @@ class A<T> {}
 class B<T> {}
 
 class C<T1,T2> {
-	var a:A<T
+	var a:A<T1>;
+	var b:B<T2>;
+
+	function f():A<T1> return null;
+}
+
+class D extends C<String, String> {
+	override function f():A<String> return null;
+}
