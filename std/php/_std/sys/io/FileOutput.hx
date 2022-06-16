2f4ad0a8589f7fcf1cf481779b297628d2875474
didipace <108588596+didipace@users.noreply.github.com>
2022-06-16 11:24:11 +0000

    add configuration file

--- std/php/_std/sys/io/FileOutput.hx
+++ std/php/_std/sys/io/FileOutput.hx
@@ -27,4 +27,60 @@ import php.Global.*;
 import php.Const.*;
 
 @:coreApi
-class FileOutput exten
+class FileOutput extends haxe.io.Output {
+	private var __f:Resource;
+
+	function new(f:Resource):Void {
+		__f = f;
+	}
+
+	public override function writeByte(c:Int):Void {
+		var r = fwrite(__f, chr(c));
+		if (r == false)
+			throw haxe.io.Error.Custom('An error occurred');
+	}
+
+	public override function writeBytes(b:haxe.io.Bytes, p:Int, l:Int):Int {
+		var s = b.getString(p, l);
+		if (feof(__f))
+			throw new haxe.io.Eof();
+		var r = fwrite(__f, s, l);
+		if (r == false)
+			throw haxe.io.Error.Custom('An error occurred');
+		return r;
+	}
+
+	public override function flush():Void {
+		var r = fflush(__f);
+		if (r == false)
+			throw haxe.io.Error.Custom('An error occurred');
+	}
+
+	public override function close():Void {
+		super.close();
+		if (__f != null)
+			fclose(__f);
+	}
+
+	public function seek(p:Int, pos:FileSeek):Void {
+		var w;
+		switch (pos) {
+			case SeekBegin:
+				w = SEEK_SET;
+			case SeekCur:
+				w = SEEK_CUR;
+			case SeekEnd:
+				w = SEEK_END;
+		}
+		var r = fseek(__f, p, w);
+		if (r == -1)
+			throw haxe.io.Error.Custom('An error occurred');
+	}
+
+	public function tell():Int {
+		var r = ftell(__f);
+		if (r == false)
+			throw haxe.io.Error.Custom('An error occurred');
+		return r;
+	}
+}
---
+++ tests/misc/projects/Issue6992/Main.hx
@@ -0,0 +1,8 @@
+
+class Main {
+	static public function main() {
+		foo(12);
+	}
+
+	inline static function foo( b ) { foo(b); }
+}
