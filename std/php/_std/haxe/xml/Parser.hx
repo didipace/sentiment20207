51c825a71e48c5511d69aeb7ec73c67ad09509c5
didipace <108588596+didipace@users.noreply.github.com>
2022-07-18 06:54:32 +0000

    Fix test path

--- std/php/_std/haxe/xml/Parser.hx
+++ std/php/_std/haxe/xml/Parser.hx
@@ -97,4 +97,117 @@ class Parser {
 	}
 
 	static public function parse(str:String, strict = false) {
-		var doc = Xml.createDocument(
+		var doc = Xml.createDocument();
+		doParse(str, strict, 0, doc);
+		return doc;
+	}
+
+	static function doParse(str:NativeString, strict:Bool, p:Int = 0, ?parent:Xml):Int {
+		var xml:Xml = null;
+		var state = S.BEGIN;
+		var next = S.BEGIN;
+		var aname = null;
+		var start = 0;
+		var nsubs = 0;
+		var nbrackets = 0;
+		var c = str.fastCodeAt(p);
+		var buf:NativeString = '';
+		// need extra state because next is in use
+		var escapeNext = S.BEGIN;
+		var attrValQuote = -1;
+		inline function addChild(xml:Xml) {
+			parent.addChild(xml);
+			nsubs++;
+		}
+		while (!StringTools.isEof(c)) {
+			switch (state) {
+				case S.IGNORE_SPACES:
+					switch (c) {
+						case '\n'.code, '\r'.code, '\t'.code, ' '.code:
+						default:
+							state = next;
+							continue;
+					}
+				case S.BEGIN:
+					switch (c) {
+						case '<'.code:
+							state = S.IGNORE_SPACES;
+							next = S.BEGIN_NODE;
+						default:
+							start = p;
+							state = S.PCDATA;
+							continue;
+					}
+				case S.PCDATA:
+					if (c == '<'.code) {
+						buf = buf.addSub(str, start, p - start);
+						var child = Xml.createPCData(buf);
+						buf = '';
+						addChild(child);
+						state = S.IGNORE_SPACES;
+						next = S.BEGIN_NODE;
+					} else if (c == '&'.code) {
+						buf = buf.addSub(str, start, p - start);
+						state = S.ESCAPE;
+						escapeNext = S.PCDATA;
+						start = p + 1;
+					}
+				case S.CDATA:
+					if (c == ']'.code && str.fastCodeAt(p + 1) == ']'.code && str.fastCodeAt(p + 2) == '>'.code) {
+						var child = Xml.createCData(str.substr(start, p - start));
+						addChild(child);
+						p += 2;
+						state = S.BEGIN;
+					}
+				case S.BEGIN_NODE:
+					switch (c) {
+						case '!'.code:
+							if (str.fastCodeAt(p + 1) == '['.code) {
+								p += 2;
+								if (Global.strtoupper(str.substr(p, 6)) != "CDATA[")
+									throw new XmlParserException("Expected <![CDATA[", str, p);
+								p += 5;
+								state = S.CDATA;
+								start = p + 1;
+							} else if (str.fastCodeAt(p + 1) == 'D'.code || str.fastCodeAt(p + 1) == 'd'.code) {
+								if (Global.strtoupper(str.substr(p + 2, 6)) != "OCTYPE")
+									throw new XmlParserException("Expected <!DOCTYPE", str, p);
+								p += 8;
+								state = S.DOCTYPE;
+								start = p + 1;
+							} else if (str.fastCodeAt(p + 1) != '-'.code || str.fastCodeAt(p + 2) != '-'.code)
+								throw new XmlParserException("Expected <!--", str, p);
+							else {
+								p += 2;
+								state = S.COMMENT;
+								start = p + 1;
+							}
+						case '?'.code:
+							state = S.HEADER;
+							start = p;
+						case '/'.code:
+							if (parent == null)
+								throw new XmlParserException("Expected node name", str, p);
+							start = p + 1;
+							state = S.IGNORE_SPACES;
+							next = S.CLOSE;
+						default:
+							state = S.TAG_NAME;
+							start = p;
+							continue;
+					}
+				case S.TAG_NAME:
+					if (!isValidChar(c)) {
+						if (p == start)
+							throw new XmlParserException("Expected node name", str, p);
+						xml = Xml.createElement(str.substr(start, p - start));
+						addChild(xml);
+						state = S.IGNORE_SPACES;
+						next = S.BODY;
+						continue;
+					}
+				case S.BODY:
+					switch (c) {
+						case '/'.code:
+							state = S.WAIT_END;
+						case 
