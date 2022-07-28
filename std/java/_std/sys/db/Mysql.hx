85ec8ea0b5774b2cdea5da20851dac24ff991754
didipace <108588596+didipace@users.noreply.github.com>
2022-07-28 20:11:20 +0000

    performance tweaks

--- std/java/_std/sys/db/Mysql.hx
+++ std/java/_std/sys/db/Mysql.hx
@@ -16,44 +16,4 @@
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- * DEALINGS IN THE SOFTWARE.
- */
-
-package sys.net;
-
-import cpp.NativeSocket;
-
-@:coreApi
-class Host {
-	public var host(default, null):String;
-
-	public var ip(default, null):Int;
-
-	private var ipv6(default, null):haxe.io.BytesData;
-
-	public function new(name:String):Void {
-		host = name;
-		try {
-			ip = NativeSocket.host_resolve(name);
-		} catch (e:Dynamic) {
-			ipv6 = NativeSocket.host_resolve_ipv6(name);
-		}
-	}
-
-	public function toString():String {
-		return ipv6 == null ? NativeSocket.host_to_string(ip) : NativeSocket.host_to_string_ipv6(ipv6);
-	}
-
-	public function reverse():String {
-		return ipv6 == null ? NativeSocket.host_reverse(ip) : NativeSocket.host_reverse_ipv6(ipv6);
-	}
-
-	public static function localhost():String {
-		return NativeSocket.host_local();
-	}
-
-	static function __init__():Void {
-		NativeSocket.socket_init();
-	}
-}
+ * FROM, OUT OF OR IN CONNECTION WI
