ca7b1167537ef90a0962c2b847ccaa4915800f46
didipace <108588596+didipace@users.noreply.github.com>
2022-07-31 08:38:01 +0000

    add coveralls support

--- std/java/_std/sys/db/Mysql.hx
+++ std/java/_std/sys/db/Mysql.hx
@@ -16,4 +16,41 @@
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WI
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+package sys.db;
+
+class Mysql {
+	static var init = false;
+
+	public static function connect(params:{
+		host:String,
+		?port:Int,
+		user:String,
+		pass:String,
+		?socket:String,
+		?database:String
+	}):sys.db.Connection {
+		if (!init) {
+			java.lang.Class.forName("com.mysql.jdbc.Driver");
+			init = true;
+		}
+		var url = new StringBuf();
+		url.add('jdbc:mysql:');
+		if (params.socket != null) {
+			url.add(params.socket);
+		} else {
+			url.add('//');
+			url.add(params.host);
+			if (params.port != null)
+				url.add(':${params.port}');
+		}
+		if (params.database != null) {
+			url.add('/${params.database}');
+		}
+		var cnx = java.sql.DriverManager.getConnection(url.toString(), params.user, params.pass);
+		return java.db.Jdbc.create(cnx);
+	}
+}
