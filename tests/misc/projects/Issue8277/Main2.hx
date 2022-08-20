134f85dcd38c67160581fe4c4b1f0b9f1abde687
didipace <108588596+didipace@users.noreply.github.com>
2022-08-20 07:34:18 +0000

    update to MIT license

--- tests/misc/projects/Issue8277/Main2.hx
+++ tests/misc/projects/Issue8277/Main2.hx
@@ -2,3 +2,16 @@ typedef C = { foo: Int }
 
 enum E {
 	A(v:C);
+}
+
+class Main {
+	static final ident:C = null;
+
+	static function f(e:E):Int {
+		return switch e {
+			case A(ident): 0;
+		}
+	}
+
+	static function main() {}
+}
