91c9114edc6c4fc3a09c3144aa614167caa5894f
didipace <108588596+didipace@users.noreply.github.com>
2022-10-24 08:40:19 +0000

    updated by automatic grading script

--- std/js/Lib.hx
+++ std/js/Lib.hx
@@ -16,4 +16,103 @@
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+package js;
+
+/**
+	Platform-specific JavaScript Library. Provides some platform-specific functions
+	for the JavaScript target.
+**/
+class Lib {
+	/**
+		Inserts a 'debugger' statement that will make a breakpoint if a debugger is available.
+	**/
+	public static inline function debug() {
+		js.Syntax.code("debugger");
+	}
+
+	/**
+		Inserts an `import` expression that loads JavaScript object from
+		a module or file specified in the `module` argument.
+	**/
+	public static inline function dynamicImport(module:String):js.lib.Promise<Dynamic> {
+		return js.Syntax.code("import({0})", module);
+	}
+
+	/**
+		Display an alert message box containing the given message.
+		@deprecated Use Browser.alert() instead.
+	**/
+	@:deprecated("Lib.alert() is deprecated, use Browser.alert() instead")
+	public static function alert(v:Dynamic) {
+		js.Syntax.code("alert")(@:privateAccess js.Boot.__string_rec(v, ""));
+	}
+
+	public static inline function eval(code:String):Dynamic {
+		return js.Syntax.code("eval")(code);
+	}
+
+	/**
+		Inserts a `require` expression that loads JavaScript object from
+		a module or file specified in the `module` argument.
+
+		This is only supported in environments where `require` function
+		is available, such as Node.js or RequireJS.
+	**/
+	extern public static inline function require(module:String):Dynamic {
+		return js.Syntax.code("require")(module);
+	}
+
+	/**
+		Native JavaScript `parseInt` function.
+
+		Its specification is different from `Std.parseInt`, so one
+		might want to access the native one.
+	**/
+	public static var parseInt(get, never):(string:String, ?radix:Int) -> Float;
+
+	extern static inline function get_parseInt():(string:String, ?radix:Int) -> Float {
+		return js.Syntax.code("parseInt");
+	}
+
+	/**
+		Returns JavaScript `undefined` value.
+
+		Note that this is only needed in very rare cases when working with external JavaScript code.
+
+		In Haxe, `null` is used to represent the absence of a value.
+	**/
+	public static var undefined(get, never):Dynamic;
+
+	static inline function get_undefined():Dynamic {
+		return js.Syntax.code("undefined");
+	}
+
+	/**
+		`nativeThis` is the JavaScript `this`, which is semantically different
+		from the Haxe `this`. Use `nativeThis` only when working with external
+		JavaScript code.
+
+		In Haxe, `this` is always bound to a class instance.
+		In JavaScript, `this` in a function can be bound to an arbitrary
+		variable when the function is called using `func.call(thisObj, ...)` or
+		`func.apply(thisObj, [...])`.
+
+		Read more at https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/this
+	**/
+	public static var nativeThis(get, never):Dynamic;
+
+	extern static inline function get_nativeThis():Dynamic {
+		return js.Syntax.code("this");
+	}
+
+	/**
+		Call JavaScript `typeof` operator on the `o` value
+		and return a string representing the JavaScript type of a value.
+
+		Read more at https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof
+	**/
+	exte
