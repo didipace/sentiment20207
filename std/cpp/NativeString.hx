61912a8f812766d463fa2a8da0d8af61dbb6afa9
didipace <108588596+didipace@users.noreply.github.com>
2022-11-04 14:09:04 +0000

    OAK-4400 - Correlate index with the index definition used to build it

--- std/cpp/NativeString.hx
+++ std/cpp/NativeString.hx
@@ -27,3 +27,47 @@ extern class NativeString {
 		return untyped inString.raw_ptr();
 	}
 	static inline function c_str(inString:String):ConstPointer<Char> {
+		return cpp.ConstPointer.fromPointer(untyped inString.c_str());
+	}
+	static inline function fromPointer(inPtr:ConstPointer<Char>):String {
+		return untyped __global__.String(inPtr.ptr);
+	}
+	static inline function fromGcPointer(inPtr:ConstPointer<Char>, inLen:Int):String {
+		return untyped __global__.String(inPtr.ptr, inLen);
+	}
+
+	@:native("__hxcpp_parse_float")
+	public static function parseFloat(inString:String):Float;
+
+	@:native("__hxcpp_parse_substr_float")
+	public static function parseSubstrFloat(inString:String,start:Int, length:Int):Float;
+
+	// Will return 0 for invalid string
+	@:native("__hxcpp_parse_substr_int")
+	public static function parseInt(inString:String):Int;
+
+	// Will return 0 for invalid string
+	@:native("__hxcpp_parse_substr_int")
+	public static function parseSubstrInt(inString:String,start:Int, length:Int):Int;
+
+	@:native("_hx_string_compare")
+	static function compare(inString0:String, inString1:String):Int;
+
+	@:native("_hx_utf8_char_code_at")
+	static function utf8CharCodeAt(inString:String, inIndex:Int):Int;
+
+	@:native("_hx_utf8_length")
+	static function utf8Length(inString:String):Int;
+
+	@:native("_hx_utf8_is_valid")
+	static function utf8IsValid(inString:String):Bool;
+
+	@:native("_hx_utf8_sub")
+	static function utf8Sub(inString:String, charStart:Int, inLen:Int):String;
+
+	@:native("_hx_string_create")
+	static function fromPointerLen(inPtr:ConstPointer<Char>, len:Int):String;
+
+	@:native("_hx_utf8_decode_advance")
+	static function utf8DecodeAdvance(reference:Char):Int;
+}
