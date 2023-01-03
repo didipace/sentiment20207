e7c266f2a743ba98aa3719abf7d61b9db468d7e4
didipace <108588596+didipace@users.noreply.github.com>
2023-01-03 08:38:31 +0000

    [goal] Migrate testing to ubuntu focal

--- std/flash/Vector.hx
+++ std/flash/Vector.hx
@@ -47,3 +47,49 @@ package flash;
 
 	#if flash19
 	function insertAt(index:Int, element:T):Void;
+	#else
+	inline function insertAt(index:Int, element:T):Void {
+		(cast this).splice(index, 0, element);
+	}
+	#end
+	@:require(flash19) function removeAt(index:Int):T;
+
+	inline static function ofArray<T>(v:Array<T>):Vector<T> {
+		return untyped __vector__(v);
+	}
+
+	inline static function convert<T, U>(v:Vector<T>):Vector<U> {
+		return untyped __vector__(v);
+	}
+
+	/**
+		Get a run-time value referencing the `Vector` class with concrete type parameters.
+
+		Normally in Haxe, for most of the types, type parameters are eliminated at run-time,
+		so there is no way to check if a value is of a type with specific type parameters.
+
+		However, on the Flash target, the `flash.Vector<T>` values carry type parameter
+		information at run-time all the type-checks (such as `Std.isOfType` and `Std.downcast`) on them
+		must be done using a `Class<T>` value that also carries the type parameters. However,
+		Haxe syntax does not allow creating such values and this function exists to mitigate
+		this limitation.
+
+		It should be used as such:
+		```haxe
+		var specificVectorType:Class<Vector<Int>> = Vector.typeReference();
+		trace(Std.isOfType(vec, specificVectorType));
+		```
+		or using the type-check syntax:
+		```haxe
+		trace(Std.isOfType(vec, (Vector.typeReference() : Class<Vector<Int>>)));
+		```
+
+		It's also helpful when working with native Flash libraries, that receive Class instances:
+		```haxe
+		new Signal((Vector.typeReference() : Class<Vector<Int>>));
+		```
+	**/
+	inline static function typeReference<T>():Class<Vector<T>> {
+		return untyped __vector__();
+	}
+}
